754e71a99b03d52caf7ae64be0b25f687c5fa7e7
huwialt <122467195+nuegoX@users.noreply.github.com>
2023-04-26 09:26:05 +0200

    1

--- jjs/src/App.tsx
+++ jjs/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './index.css';
+import { Routes } from 'react-router-dom';
+import Start from './Pages/Start';
 
 function App() {
   return (
     <>
 
+      <Routes>
+        
+      </Routes>
     </>
   );
 }
---
+++ jjs/src/Components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const Navbar = () => {
+
+    const [nav, setNav] = useState(false)
+    
+  return (
+    <>
+        <nav className='flex justify-center align-middle'>
+            
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
