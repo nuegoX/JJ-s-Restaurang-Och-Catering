52cf409c5b0d2a0fc22fe53ccc118a469fa0806d
huwialt <122467195+nuegoX@users.noreply.github.com>
2023-05-10 09:30:45 +0200

    Update Navbar.tsx

--- jjs/src/Components/Navbar.tsx
+++ jjs/src/Components/Navbar.tsx
@@ -31,9 +31,28 @@ const Navbar = () => {
     {onmobile ? (
     <nav className='flex w-full h-fit justify-between items-center bg-black'>
       <Link to='/'><img src="https://i.ibb.co/MRksVVP/LOGO.png" width='70px' height='auto' alt="LOGO" className='ml-3' /></Link>
-      
+      <img alt="svgImg" onClick={handleNav} className={`w-[40px] mr-3 transition-all ${nav && 'rotate-[130deg]'}`} src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjBweCIgeT0iMHB4Igp3aWR0aD0iNjQiIGhlaWdodD0iNjQiCnZpZXdCb3g9IjAgMCA2NCA2NCIKc3R5bGU9ImZpbGw6I0ZGRkZGRjsiPgo8cGF0aCBkPSJNNyAyOXY1bDUwIDF2LTdMNyAyOXpNNyAxMXY2aDUwdi02SDd6TTcgNDZ2N2w1MC0xdi01TDcgNDZ6Ij48L3BhdGg+Cjwvc3ZnPg=="/>
+      {nav && (
+        <div className={`absolute flex flex-col justify-center items-center right-0 ${nav ? "top-16" : "top-0"} w-full bg-black rounded-lg py-2 px-4`}>
+          <div className="flex flex-col justify-center items-center w-full rounded-lg py-2 px-4 bg-gradient-to-b from-yellow-300 to-yellow-500">
+            <ul className='flex flex-col gap-5'>
+              <li>START</li>
+              <li>Om oss</li>
+              <li>Lunchmeny</li>
+              <li>Catering</li>
+              <li>Bildgalleri</li>
+              <li>Hitta till oss</li>
+              <li>Kontakta oss</li>
+            </ul>
+          </div>
+       </div>
+      )}
     </nav>
+    
+    // ^ On mobile device
     ) : (
+    // Bigger screen navbar
+    
     <nav>
       <p>Laptop</p>
       <a href="https://ibb.co/pdRKWxw"><img src="https://i.ibb.co/YhydD3Q/JJS.png" alt="JJS" width='70px' className='mr-3' /></a>
